9ebb6207165cd3c05d4ed7992a7adfe61b00044c
Carlos Eduardo <carlosedu9090466@gmail.com>
2023-10-18 22:40:11 -0300

    css_login

--- resources/views/layouts/mainLogin.blade.php
+++ resources/views/layouts/mainLogin.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 
 </head>
 
